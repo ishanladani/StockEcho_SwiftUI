6e54c308cb546c419bee9a0aff0e7abdcef929bc
Ishan Ladani <ishanladani16@gmail.com>
2026-03-03 19:58:02 +0530

    Project Setup

---
+++ StockEchoApp/StockEchoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockEchoAppApp.swift
+//  StockEchoApp
+//
+//  Created by ISHAN LADANI on 27/02/26.
+//
+
+import SwiftUI
+
+@main
+struct StockEchoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StockEchoAppUITests/StockEchoAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  StockEchoAppUITestsLaunchTests.swift
+//  StockEchoAppUITests
+//
+//  Created by ISHAN LADANI on 27/02/26.
+//
+
+import XCTest
+
+final class StockEchoAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
